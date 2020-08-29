56c2dbd89512ce8ee2efefdbf0c89f69f54eafa3
Ryson <rysonshalvis@gmail.com>
2020-08-28 23:27:14 -0600

    add open video function

--- sources/main.js
+++ sources/main.js
@@ -3,11 +3,12 @@ const hamburgerIcon = document.querySelector("#hamburger-icon");
 const hamburgerInside1 = document.querySelector(".ham");
 const hamburgerInside2 = hamburgerInside1.nextElementSibling;
 const hamburgerInside3 = hamburgerInside2.nextElementSibling;
+
 let hello = true;
-function showDropDown () {
+function toggleDropDown () {
     console.log('hello');
     // checking if hello is true will fix the bug thats clicking twice
-    if (dropDown.style.display === 'none') {
+    if (hello === true) {
         dropDown.style.display = 'block';
         hamburgerInside1.style.backgroundColor = 'black';
         hamburgerInside2.style.backgroundColor = 'black';
@@ -29,4 +30,24 @@ function showDropDown () {
     }
 };
 
-hamburgerIcon.addEventListener('click',showDropDown);
+hamburgerIcon.addEventListener('click',toggleDropDown);
+
+
+const popUpVideo = document.querySelector ('.play-video-container');
+const playButton = document.querySelector('#video-btn');
+const exitButton = document.querySelector('#x-button');
+let goodBye = true;
+function openVideo () {
+        popUpVideo.style.display = 'block';
+        console.log('hello play btn') 
+        goodBye = false;
+ 
+};
+
+function closeVideo () {
+    popUpVideo.style.display = 'none';
+        goodBye = true;
+}
+
+playButton.addEventListener('click', openVideo);
+exitButton.addEventListener('click', closeVideo);
